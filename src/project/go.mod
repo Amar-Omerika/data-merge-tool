18775e48f68ca937560bb79cb9460ee5cb3001d1
Amar-Omerika <amar.omerika@edu.fit.ba>
2025-04-15 17:42:32 +0200

    feat: init test

---
+++ src/project/go.mod
@@ -0,0 +1,3 @@
+module github.com/Amar-Omerika/data-merge-tool
+
+go 1.24.2
---
+++ src/project/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// main is the entry point of the application.
+// When you run this application, it will print a message.
+func main() {
+    fmt.Println("Hello, data-merge-tool!")
+}
